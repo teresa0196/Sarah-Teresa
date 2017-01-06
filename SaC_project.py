7a5be45f45943058ca802bdd7821c607bf4a4092
Teresa Jeon <teresajeon@seoulchristian.org>
2017-01-06 10:45:00 +0900

    1

--- SaC_project.py
+++ SaC_project.py
@@ -1,1 +1,7 @@
-# Teresa and Sarah's project for the 
+# Teresa and Sarah's Stretch Project 
+# Needs to be : 
+# Sort a list of names alphabetically by first name
+# Sort a list of names alphabetically by last name
+# Sort a list of names by total length
+# Reverse the sort of a list of names
+# Correctly capitalize a list of names
